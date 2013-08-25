98b1f9ab0202225a47b94939f9fb0abaa5cea8d8
Joe Cross <joe.mcross@gmail.com>
2013-08-24 21:16:56 -0700

    Add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,25 @@
+from invoke import task, run
+
+_run = run
+
+def run(*args, **kwargs):
+    kwargs.update(echo=True)
+    return _run(*args, **kwargs)
+
+@task
+def clean():
+    run("find . -name '*.so' -delete")
+    run("rm -rf kmeans.egg-info")
+    run("rm -rf build/")
+
+@task('clean')
+def build():
+    run("python setup.py develop")
+
+@task('clean')
+def pypi():
+    run('python setup.py sdist upload')
+
+@task('clean')
+def test():
+    run('tox')
